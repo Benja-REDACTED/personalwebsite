b2ddc849ac1a53aec516a197635364bb427fa191
Benja-REDACTED <bjsheriff111@gmail.com>
2026-04-30 16:54:01 -0500

    added dialogue submission

--- fish.js
+++ fish.js
@@ -1,7 +1,14 @@
 let animationTimer = 0
 let bubbles = []
 let canvas = null
-possibleReactions = {
+let tickRate = 60
+let fishPos = [100, 100]
+function distance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow(y2 - y1, 2))
+}
+
+
+let possibleReactions = {
     "screw you": 5,
     "bluble":80,
     "blub":90,
@@ -23,14 +30,22 @@ possibleReactions = {
     "water is in the ocean :O": 20,
     "manta rays can grow up to 23 feet long! but my cousin is one and he's nice :)":20,
     "parrot fish make good pets because they are social":20,
-    "ZAYNE MICHAEL STONE FROM OWENSBORO KENTUCKY PEED IN THE OCEAN ONCE!!! ....i can still taste it :(.....":20,
+    "ZAYNE MICHAEL STONE FROM OWENSBORO KENTUCKY PEED IN THE OCEAN ONCE!!! ....i can still taste it :(.....":10,
     "leopard sharks are very protective of their pups! chris hansen is actually a leopard shark!":1,
     "leopard sharks are very protective of their pups!":20,
     "there are only 20 vaquitas left in the world":20,
     "ELLA NEAL FROM OWENSBORO KENTUCKY LOVES MONEY":1,
-    "oceans arenmt desrt":5
+    "oceans arenmt desrt":10,
+    "I teach you and me teach i? pokemon?":5,
+    "It teach you and you teach me!!! Pokeeemonnnn": 15,
+    "there is no spoon":10,
+    "wow that back ground has a lot of colors. Rainbow? asriel dreamur refrence?":2,
+    "GO PLAY OUTER WILDS":10,
+    "there are way too many command and conquer games.":5,
+    "GO PLAY RIMWORLD":10
     }
-    weightedPossibleReactions = []
+    let weightedPossibleReactions = []
+    let i = 0
     for (i in possibleReactions) {
         let counter = 0
         while (counter < possibleReactions[i]){
@@ -38,8 +53,6 @@ possibleReactions = {
             counter += 1
         }
     }
-    console.log(weightedPossibleReactions)
-
 
 
 function draw(){
@@ -54,24 +67,30 @@ function draw(){
     fish.src = "./fihstuff/fih.png"
     bubble.src = "./fihstuff/bubble.png"
 
+    fishPos = [0.34*canvas.width, Math.sin(animationTimer/200)*canvas.height*0.3+0.375*canvas.height]
 
     ctx.drawImage(tankBackground, 0, 0, canvas.width, canvas.height)
-    ctx.drawImage(fish, 0.34*canvas.width, Math.sin(animationTimer/200)*canvas.height*0.3+0.375*canvas.height, 0.32*canvas.width, 0.25*canvas.height);
+    ctx.drawImage(fish, fishPos[0], fishPos[1], 0.32*canvas.width, 0.25*canvas.height);
+    //ctx.drawImage(fish, fishPos[0], fishPos[1], 0.32*canvas.width, 0.25*canvas.height);
 
-    counter = 0
+    let counter = 0
     while (counter < bubbles.length){
         bubbles[counter][1] -= bubbles[counter][2]
         ctx.drawImage(bubble, bubbles[counter][0], bubbles[counter][1], 20, 20)
+
 
         counter += 1
     }
     animationTimer += 1
     }
-setInterval(draw, 16.6);
+setInterval(draw, 1000/tickRate);
 
 function button() {
     const fishTranslator = document.getElementById("live fish reaction");
 
     fishTranslator.textContent =  "live fish reaction: " + weightedPossibleReactions[Math.floor(Math.random() * weightedPossibleReactions.length)]
-    bubbles.push([Math.random()*10+0.37*canvas.width, Math.sin(animationTimer/200)*canvas.height*0.3+0.375*canvas.height, Math.random()+0.25])
+    bubbles.push([Math.random()*17+fishPos[0], fishPos[1], Math.random()+0.25])
 }
+
+
+
